2e4721f155f4f194ba0e3f87594ab864fd2d1e5d
= <=>
2023-07-23 09:36:23 -0400

    function finished

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,20 @@
-# your code goes here!
+import time
+
+
+def countdown(number):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        print("HAPPY NEW YEAR!")
+countdown(10)
+
+
+def countdown_with_sleep(number):
+    while number > 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        time.sleep(1)
+    print("HAPPY NEW YEAR!")
+
+
+countdown_with_sleep(10)
